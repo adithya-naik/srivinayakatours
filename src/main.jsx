62cf8bf6bcb143d886a6cff45023eaa27f0541fb
Jatoth Adithya Naik <jatothadithyanaik@gmail.com>
2025-02-28 08:03:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <App />
           <Toaster position="top-center" reverseOrder={false} />
         </BookingProvider>
-<AuthProvider/>
+</AuthProvider>
     </BrowserRouter>
      
     </HelmetProvider>
